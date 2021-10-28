8e82a63017abd97e14f728867ef820b51fded89f
Adarsh Patel <swe.adarsh@gmail.com>
2021-10-28 00:35:44 -0400

    faster  Longest Substring Without Repeating Characters

--- python/LongestSubstringWithoutReoeatingCharacters.py
+++ python/LongestSubstringWithoutReoeatingCharacters.py
@@ -1,23 +1,44 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+        
+#         currMax = 0 
+        
+#         if len(s) == 0:
+#             return 0
+        
+#         for i in range(len(s)):
+#             currString = []
+#             next = i
+#             foundRepeat = False            
+            
+#             while((not foundRepeat) and (next < len(s))):
+#                 if s[next] in currString: 
+#                     foundRepeat = True
+#                 else:    
+#                     currString.append(s[next])
+#                     next+=1
+                    
+#             if(len(currString) > currMax):
+#                 currMax = len(currString)
+
+#         return currMax
+
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         
-        currMAX = 0 
+        max_length = 0 
+        start = 0
+        end = 0
         
-        if len(s) == 0:
-            return 0
-        
-        for i in range(len(s)):
-            currString = []
-            next = i+1 
-            foundRepeat = False
+        substringWindow = [] 
 
-            while( not foundRepeat and next < len(s)):
-                if(s[i] == s[next]):
-                    foundRepeat = True
-                else:
-                    currString.append(s[i])
-                
-            if(len(currString) > currMax):
-                currMax = len(currString)
-
-        return currMax
+        while(end < len(s)):
+            if(not s[end] in substringWindow):
+                substringWindow.append(s[end])
+                end+=1
+                max_length = max(max_length, len(substringWindow))
+            else:
+                substringWindow.remove(s[start])
+                start +=1
+        return max
