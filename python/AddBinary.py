6c4f3df9f0974ed60907b9dad48b23a78bf6f8cb
Adarsh Patel <swe.adarsh@gmail.com>
2021-10-29 03:20:15 -0400

    faster add binary

--- python/AddBinary.py
+++ python/AddBinary.py
@@ -1,24 +1,19 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        longer = a if len(a) >= len(b) else b
-        shorter = a if len(a) < len(b) else b
-    
         s=""
         sum=0
-
-        LLen = len(longer)
-        SLen = len(shorter)
-
-        while(LLen >=0):
-            if(SLen >= 0):
-                sum = sum + int(longer[LLen]) + int(shorter[SLen])
-            else: 
-                sum = sum + int(longer[LLen])
+        i = len(a) - 1 
+        j = len(b) - 1 
+        while(i >=0 or j >=0):
+            if(i>=0):
+                sum = sum + int(a[i])
+            if(j>=0):
+                sum = sum + int(b[j])
             s = str(sum % 2 ) + s 
             sum = sum // 2
 
-            LLen -=1 
-            SLen -=1 
+            i -= 1 
+            j -= 1
  
         if(sum != 0):
             s = str(sum) + s 
