dd2b3d0f6e47339e94bf705c06e4d11d3ef453da
Adarsh Patel <swe.adarsh@gmail.com>
2021-10-29 02:36:17 -0400

    faster 3Sum

--- python/3Sum.py
+++ python/3Sum.py
@@ -1,23 +1,52 @@
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         if(len(nums) < 3):
+#             return []
+
+#         x = [] 
+#         nums.sort()
+
+#         for i in range(len(nums)):
+#             target = -1*nums[i]
+
+#             j = i + 1 
+#             d={}
+#             while(j<len(nums)):
+#                 if((target-nums[j]) in d):
+#                     newArr = [nums[i], target-nums[j], nums[j]]
+#                     if not(newArr in x):
+#                         x.append(newArr)
+#                 else:
+#                     d[nums[j]] = j    
+#                 j+=1
+
+#         return x
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        if(len(nums) < 3):
-            return []
-
-        x = [] 
+        output = []
         nums.sort()
 
-        for i in range(len(nums)):
-            target = -1*nums[i]
+        for i, x in enumerate(nums):
+            if(i>0 and x == nums[i-1]):
+                continue
 
-            j = i + 1 
-            d={}
-            while(j<len(nums)):
-                if((target-nums[j]) in d):
-                    newArr = [nums[i], target-nums[j], nums[j]]
-                    if not(newArr in x):
-                        x.append(newArr)
+            left = i+1
+            right = len(nums) -1 
+
+            while(left<right):
+                threeSum = x + nums[left] + nums[right]
+                if(threeSum > 0):
+                    right -=1
+                elif(threeSum <0):
+                    left +=1 
                 else:
-                    d[nums[j]] = j    
-                j+=1
+                    output.append([x, nums[left], nums[right]])
+                    left += 1 
+                    while(nums[left-1] == nums[left] and left<right):
+                        left+=1
+        return output
 
-        return x
+
+
+
