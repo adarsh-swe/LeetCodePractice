0a4b87d686e740d68e02fef8fc5e2da102bc73f1
Adarsh Patel <swe.adarsh@gmail.com>
2021-10-30 01:59:46 -0400

    opitimized subarray sum equals k

--- python/SubarraySumEqualsK.py
+++ python/SubarraySumEqualsK.py
@@ -3,6 +3,8 @@ class Solution:
         
         """
         BruteForce 
+        O(n^2)
+        """
         """
         count = 0 
         for i in range(len(nums)):
@@ -12,23 +14,27 @@ class Solution:
                 if(sum==k):
                     count+=1
         return count
+        """
 
+        """
+        Optimized Solution
+        O(n)
+        """
+    
+        count = 0 
+        sum = 0
+        occur = {
+            0:1
+        }
 
-        # left = 0
-        # right = 0
-        # count = 0 
-        # sum = 0
+        for i in range(len(nums)):
+            sum += nums[i]
+            if (sum-k) in occur:
+                count += occur[sum-k]
+            if sum in occur: 
+                occur[sum] += 1
+            else:
+                occur[sum] = 1
 
-        # while(right < len(nums)):
-        #     if sum == k:
-        #         count+=1 
-        #         sum-=nums[left]
-        #         left+=1
-        #     elif sum < k:
-        #         right +=1 
-        #         sum += nums[right]
-        #     elif sum > k:
-        #         sum-=nums[left]
-        #         left+=1
-
-        # return count 
+        
+        return count  
